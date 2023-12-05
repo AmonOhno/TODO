6a91401b081eed1119bb2fd048d8b36d17223efa
Amon <amon@Mac-mini.local>
2023-12-05 22:46:50 +0900

    Successed to addict li.classList. But, failed to run function "reloadCSS".

--- text_job.js
+++ text_job.js
@@ -1,33 +1,46 @@
 
 //各[done]ボタンのクリックイベントを取得
-const buttonDone = document.querySelectorAll('.btn-done');
+const buttonDone = document.querySelectorAll(".btn-done");
 [...buttonDone].forEach((w_button) => {
-    w_button.addEventListener('click', function() {todoEnvChangeClass(w_button)});
-});
+    w_button.addEventListener("click", function() {todoEnvChangeClass(w_button)});
+})
 
 //各[done]ボタンのクリックイベントを取得
-const buttonCurrnt = document.querySelectorAll('.btn-current');
+const buttonCurrnt = document.querySelectorAll(".btn-current");
 [...buttonCurrnt].forEach((w_button) => {
-    w_button.addEventListener('click', function() {todoEnvChangeClass(w_button)});
-});
+    w_button.addEventListener("click", function() {todoEnvChangeClass(w_button)});
+})
+
+//CSSファイルをリロード
+function reloadCSS(){
+    var links = document.getElementsByTagName('link');
+    for (var i = 0; i < links.length; i++) {
+        if (links[i].rel === 'stylesheet') {
+            links[i].disabled = true;
+            links[i].disabled = false;
+        }
+    }
+}
+
 
 
 //<li>タグのクラス属性をtodo-doneに更新する
 function todoEnvChangeClass(e){
     console.log(e);
-    var li = e.closest('li');
-    if (e.classList.contains('btn-done')) {
-        li.classList.remove('txt-default');
-        li.classList.remove('txt-current');
-        li.classList.add('txt-done'); //ここがHTMLで読み込んでくれない
+    var li = e.closest("li");
+    if (e.classList.contains("btn-done")) {
+        li.classList.remove("txt-default");
+        li.classList.remove("txt-current");
+        li.classList.add("txt-done");
     } 
-    else if (e.classList.contains('btn-current')) {
-        li.classList.remove('txt-default');
-        li.classList.remove('txt-done');
-        li.classList.add('txt-current'); //ここがHTMLで読み込んでくれない
-    };
+    else if (e.classList.contains("btn-current")) {
+        li.classList.remove("txt-default");
+        li.classList.remove("txt-done");
+        li.classList.add("txt-current");
+    }
 
-};
+    reloadCSS(); //CSSリロード関数が走らない
 
+}
 
 
