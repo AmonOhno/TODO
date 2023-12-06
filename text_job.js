d080b470bc9060ad8972536ee4e3af0342dc6e10
Amon <amon@Mac-mini.local>
2023-12-06 21:26:39 +0900

    Making new taskElement is successed. But, deploying to HTML is failed...

--- text_job.js
+++ text_job.js
@@ -1,4 +1,35 @@
 
+//[作成]ボタンのクリックイベントを取得
+const buttonMakeTask = document.querySelector(".btn-newtodo");
+buttonMakeTask.addEventListener("click", function() {
+
+    //新規<li>の構造作成処理
+    var makeTaskName = document.querySelector("#newtodo").value;
+    var newli = document.createElement("li");
+    newli.classList.add("txt-default");
+    newli.textContent = makeTaskName;
+
+    var newbuttonDone = document.createElement("button");
+    newbuttonDone.classList.add("btn-done");
+    newbuttonDone.textContent = "完了";
+    var newbuttonCurrent = document.createElement("button");
+    newbuttonCurrent.classList.add("btn-current");
+    newbuttonCurrent.textContent = "優先";
+
+    newli.appendChild(newbuttonDone);
+    newli.appendChild(newbuttonCurrent);  
+
+    console.log(newli);
+
+    //既存<li>に追加処理
+    var lastli_arr = document.querySelectorAll(".btn-done");
+    var lastli = lastli_arr.item(lastli_arr.length - 1);
+
+    newli.insertAdjacentElement("afterend", lastli); //ここが反映されない！！
+})
+
+
+
 //各[done]ボタンのクリックイベントを取得
 const buttonDone = document.querySelectorAll(".btn-done");
 [...buttonDone].forEach((w_button) => {
@@ -10,19 +41,6 @@ const buttonCurrnt = document.querySelectorAll(".btn-current");
 [...buttonCurrnt].forEach((w_button) => {
     w_button.addEventListener("click", function() {todoEnvChangeClass(w_button)});
 })
-
-//CSSファイルをリロード
-function reloadCSS(){
-    var links = document.getElementsByTagName('link');
-    for (var i = 0; i < links.length; i++) {
-        if (links[i].rel === 'stylesheet') {
-            links[i].disabled = true;
-            links[i].disabled = false;
-        }
-    }
-}
-
-
 
 //<li>タグのクラス属性をtodo-doneに更新する
 function todoEnvChangeClass(e){
@@ -38,9 +56,6 @@ function todoEnvChangeClass(e){
         li.classList.remove("txt-done");
         li.classList.add("txt-current");
     }
-
-    reloadCSS(); //CSSリロード関数が走らない
-
 }
 
 
